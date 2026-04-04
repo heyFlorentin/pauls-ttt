6b1791901cd2e481562fbddcfa751280224a2542
Phichayut 'Florentin' Sakwiset <florentin@wemake.cx>
2026-04-04 02:12:19 +0200

    fix: replace placeholder workshop collection ID with actual ID
    Use the correct workshop collection ID to ensure clients automatically download the intended addon collection. The placeholder was causing clients to attempt downloading a non-existent collection.

--- gmodserver/serverfiles/garrysmod/lua/autorun/server/workshop.lua
+++ gmodserver/serverfiles/garrysmod/lua/autorun/server/workshop.lua
@@ -1,5 +1,5 @@
 -- Automatically instruct clients to download the collection
-resource.AddWorkshop("YOUR_WORKSHOP_COLLECTION_ID")
+resource.AddWorkshop("3698746151")
 
 -- For critical individual items that fail to mount properly via collection:
 -- resource.AddWorkshop("123456789")
